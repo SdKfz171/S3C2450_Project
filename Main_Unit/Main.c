69800353c07c65fc3e62b20ba39ad3ecacc981be
DongYoung-Kim <521ab@naver.com>
2019-02-08 23:51:42 +0900

    Update Main.c

--- Main_Unit/Main.c
+++ Main_Unit/Main.c
@@ -44,6 +44,9 @@ void gpio_init(){
     // GPECON.GPIO_PIN_13 = 0x2;
     // GPECON.GPIO_PIN_12 = 0x2;
     // GPECON.GPIO_PIN_11 = 0x2;
+
+    GPBCON.GPIO_PIN_1 = ALTERNATIVE_0;
+    GPBDAT.GPIO_PIN_1 = 1;
 }
 
 void RTC_Init(){
@@ -79,11 +82,30 @@ void RTC_Tick_Init(){
 
 
 void timer0_init(){
-    rTCFG0 = (rTCFG0 & ~0xFF) | (50 - 1);   // 66,000,000 / 33
-    rTCFG1 = (rTCFG1 & ~0xF);               // 66,000,000 / 33 / 2
+    TCFG0.PRESCALER0 = (33 - 1);
+    TCFG1.MUX0 = 0;
+    // rTCFG0 = (rTCFG0 & ~0xFF) | (33 - 1);   // 66,000,000 / 33
+    // rTCFG1 = (rTCFG1 & ~0xF);               // 66,000,000 / 33 / 2
     rTCNTB0 =  (0xFFFF);                    // Max size of buffer 
     rTCON |= (0x02);                        // update TCNTB0
     rTCON = (rTCON & ~(0xf) | (1 << 3) | (1 << 0)); // auto reload & start timer0
+}
+
+// TOUT1 == BUZZER
+void timer1_init(){
+    TCFG0.PRESCALER0 = (33 - 1);
+    TCFG1.MUX1 = 0;
+    rTCNTB1 =  ((PCLK / 33 / 2) / C4) - 1;                    // Max size of buffer 
+    rTCMPB1 = rTCMPB1 / 2;
+    TCON.TIM1_START = 0;
+    TCON.TIM1_AUTO_RELOAD = 1;
+    TCON.TIM1_START = 1;
+}
+
+void Sound(uint16_t scale, int duration){
+    rTCNTB1 = ((PCLK / 33 / 2) / scale) - 1;
+    rTCMPB1 = rTCNTB1 / 2;
+    delay_ms(duration);
 }
 
 void delay_us(int us){
@@ -108,17 +130,28 @@ void delay_ms(int ms){
 void Main(){
     Uart_Init(115200);
     timer0_init();
+    timer1_init();
     RTC_Init();
     RTC_Tick_Init();
     gpio_init();
+    SPI_Init();
 
     Uart_Printf("Program Started!!\r\n");
     
     while(1){
-        GPGDAT.GPIO_PIN_4 = LOW;
-        delay_ms(1000);
-        GPGDAT.GPIO_PIN_4 = HIGH;
-        delay_ms(1000);
+        //Uart_Printf("%c", (uint8_t)SPI_RX_DATA0);
+        // SPI_Transmit('0');
+        // Uart_Printf("MOSI : %d", GPEDAT.GPIO_PIN_12);
+        // Uart_Printf("MISO : %d", GPEDAT.GPIO_PIN_13);
+        // rGPFDAT = 0x00;
+        // rGPGDAT = 0x00;
+        // rGPEDAT = 0xFF;
+        //GPGDAT.GPIO_PIN_4 = LOW;
+        Sound(C5, 100);
+        //delay_ms(1000);
+        //GPGDAT.GPIO_PIN_4 = HIGH;
+        Sound(A5, 100);
+        //delay_ms(1000);
         Uart_Printf("%x %x %x\r\n", rBCDHOUR, rBCDMIN, rBCDSEC);
     }
 }
@@ -126,4 +159,5 @@ void Main(){
 void __attribute__((interrupt("IRQ"))) RTC_TICK(void){
     ClearPending1(BIT_TICK);
     Uart_Printf("TICK INT\r\n");
+    // LCD REFRESH
 }
