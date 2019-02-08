84f6f39c54955d8a31d3ece2e4559415f75c28cd
DongYoung-Kim <521ab@naver.com>
2019-02-09 02:41:49 +0900

    Update Main.c

--- Main_Unit/Main.c
+++ Main_Unit/Main.c
@@ -45,7 +45,7 @@ void gpio_init(){
     // GPECON.GPIO_PIN_12 = 0x2;
     // GPECON.GPIO_PIN_11 = 0x2;
 
-    GPBCON.GPIO_PIN_1 = ALTERNATIVE_0;
+    GPBCON.GPIO_PIN_1 = OUTPUT;
     GPBDAT.GPIO_PIN_1 = 1;
 }
 
@@ -80,32 +80,15 @@ void RTC_Tick_Init(){
     pISR_TICK = (unsigned)RTC_TICK;
 }
 
-
 void timer0_init(){
     TCFG0.PRESCALER0 = (33 - 1);
     TCFG1.MUX0 = 0;
-    // rTCFG0 = (rTCFG0 & ~0xFF) | (33 - 1);   // 66,000,000 / 33
-    // rTCFG1 = (rTCFG1 & ~0xF);               // 66,000,000 / 33 / 2
-    rTCNTB0 =  (0xFFFF);                    // Max size of buffer 
-    rTCON |= (0x02);                        // update TCNTB0
-    rTCON = (rTCON & ~(0xf) | (1 << 3) | (1 << 0)); // auto reload & start timer0
-}
+    rTCNTB0 = (0xFFFF);
+    TCON.TIM0_MANUAL_UPDATE = 1;
 
-// TOUT1 == BUZZER
-void timer1_init(){
-    TCFG0.PRESCALER0 = (33 - 1);
-    TCFG1.MUX1 = 0;
-    rTCNTB1 =  ((PCLK / 33 / 2) / C4) - 1;                    // Max size of buffer 
-    rTCMPB1 = rTCMPB1 / 2;
-    TCON.TIM1_START = 0;
-    TCON.TIM1_AUTO_RELOAD = 1;
-    TCON.TIM1_START = 1;
-}
-
-void Sound(uint16_t scale, int duration){
-    rTCNTB1 = ((PCLK / 33 / 2) / scale) - 1;
-    rTCMPB1 = rTCNTB1 / 2;
-    delay_ms(duration);
+    TCON.TIM0_AUTO_RELOAD = 1;
+    TCON.TIM0_MANUAL_UPDATE = 0;
+    TCON.TIM0_START = 1;
 }
 
 void delay_us(int us){
@@ -114,7 +97,7 @@ void delay_us(int us){
     while(us > 0){
         last = now;
         now = rTCNTO0;
-        if(now > last){ // timer reloaded
+        if(now > last){
             diff = last + (0xFFFF - now) + 1;
         } else {
             diff = last - now;
@@ -127,31 +110,44 @@ void delay_ms(int ms){
     delay_us(ms * 1000);
 }
 
+void WT_Delay(int duration){
+    rWTCON = (37 << 8) | (3 << 3);
+    rWTDAT = duration + 10;
+    rWTCNT = duration + 10;
+    rWTCON |= (1 << 5);
+
+    while(rWTCNT > 10);
+    rWTCON = 0;   
+}
+
+void Sound(uint16_t scale, int duration){
+    while(duration > 0){
+        GPBDAT.GPIO_PIN_1 = 0;
+        WT_Delay(10000 / scale);
+        GPBDAT.GPIO_PIN_1 = 1;
+        WT_Delay(10000 / scale);
+
+        duration -= ((10000 / scale / 10) * 2) * 2;
+    }
+}
+
 void Main(){
     Uart_Init(115200);
     timer0_init();
-    timer1_init();
     RTC_Init();
     RTC_Tick_Init();
     gpio_init();
-    SPI_Init();
 
     Uart_Printf("Program Started!!\r\n");
     
+    Sound(C5, 1000);
+
     while(1){
-        //Uart_Printf("%c", (uint8_t)SPI_RX_DATA0);
-        // SPI_Transmit('0');
-        // Uart_Printf("MOSI : %d", GPEDAT.GPIO_PIN_12);
-        // Uart_Printf("MISO : %d", GPEDAT.GPIO_PIN_13);
-        // rGPFDAT = 0x00;
-        // rGPGDAT = 0x00;
-        // rGPEDAT = 0xFF;
-        //GPGDAT.GPIO_PIN_4 = LOW;
-        Sound(C5, 100);
-        //delay_ms(1000);
-        //GPGDAT.GPIO_PIN_4 = HIGH;
-        Sound(A5, 100);
-        //delay_ms(1000);
+        GPGDAT.GPIO_PIN_4 = LOW;
+        delay_ms(500);
+        GPGDAT.GPIO_PIN_4 = HIGH;
+        delay_ms(500);
+        
         Uart_Printf("%x %x %x\r\n", rBCDHOUR, rBCDMIN, rBCDSEC);
     }
 }
